d8ae3fb43838c60551adafebdecb514ef4245529
Travis Kochel <travis@scribbletone.com>
2021-01-14 12:33:00 -0800

    generate v0.16 and some build scripts

--- build-all.sh
+++ build-all.sh
@@ -16,15 +16,23 @@ else
   static_output_path="${output_path}/static"
   variable_output_path="${output_path}/variable"
 
-  mkdir -p $output_path $static_output_path $variable_output_path 
+  # mkdir -p $output_path $static_output_path $variable_output_path 
 
   # Generate VFs
   VF_full_output_path="${variable_output_path}/Kablammo-Variable-v${version}.ttf"
-  fontmake -g $glyphsSource -o variable --output-path $VF_full_output_path
+  # fontmake -g $glyphsSource -o variable --output-path $VF_full_output_path
 
   # Generate OTFS
-  fontmake -g $glyphsSource -o otf -i --output-dir $static_output_path 
+  # fontmake -g $glyphsSource -o otf -i --output-dir $static_output_path 
 
+
+  # fix Hinting
+  gftools fix-nonhinting $VF_full_output_path $VF_full_output_path
+  rm "${VF_full_output_path/.ttf/-backup-fonttools-prep-gasp.ttf}"
+  for filename in $static_output_path/*.otf; do
+    gftools fix-nonhinting $filename $filename
+    rm "${filename/.otf/-backup-fonttools-prep-gasp.otf}"
+  done
 fi
 
 
